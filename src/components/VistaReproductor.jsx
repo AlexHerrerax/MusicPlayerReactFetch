202cc275b5d661e00a2e3fd6cb40ccdda840f9e7
Alex Herrera <axherrerax@gmail.com>
2021-11-05 02:10:51 -0300

    Se arreglan detalles

--- src/components/VistaReproductor.jsx
+++ src/components/VistaReproductor.jsx
@@ -1,21 +1,23 @@
-import React, { useRef } from 'react'
+
 
 
 const VistaReproductor = ({anterior, repropausa, siguiente, listaCanciones,directo, audioRef}) => {
     return (
         <>
 
-<h2 className="text-center">Reproductor</h2>
-                        <button className="btn btn-primary" onClick={anterior}>Anterior</button>
-                        <button className="btn btn-success" onClick={repropausa}>Reproducir/Pausa</button>
-                        <button className="btn btn-primary" onClick={siguiente}>Siguiente</button>
+<h2 className="text-center mb-4 text-info">Reproductor de Música</h2>
+                        <div className="mb-3">
+                            <button className="btn btn-primary mx-2" onClick={anterior}>Anterior</button>
+                            <button className="btn btn-success mx-2" onClick={repropausa}>Reproducir/Pausa</button>
+                            <button className="btn btn-primary mx-2" onClick={siguiente}>Siguiente</button>
+                        </div>
 
                         <ul className="list-group">
                             <audio ref={audioRef} />
                             {
                                 listaCanciones.map((item, index) => (
                                     <li key={index} className="list-group-item">
-                                        <a href="#"  onClick={() => directo(index)} >{item.name}</a>
+                                        <a href="/#"  onClick={() => directo(index)} >{item.name}</a>
                                     </li>
                                 ))
                             }
